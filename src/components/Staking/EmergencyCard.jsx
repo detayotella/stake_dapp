e6d0dfeac9af02d5f07a2c5dd229ba75700fb67f
detayotella <tellaadetayo@gmail.com>
2025-09-11 10:28:22 +0100

    chore: fix the emergency card components

--- src/components/Staking/EmergencyCard.jsx
+++ src/components/Staking/EmergencyCard.jsx
@@ -19,7 +19,10 @@ function EmergencyCard() {
   return (
     <div className="bg-[var(--card-background)] rounded-lg border border-[var(--border-color)] p-6 text-center">
       <h3 className=" text-lg font-semibold text-[var(--text-color)] mb-4">Emergency</h3>
-      <button onClick={handleEmergencyWithdraw} className="mt-16 btn-secondary border-red-500 text-red-500 hover:bg-red-50 w-full h-12 rounded-lg font-semibold text-base">
+      <div>
+        <p className="bg-red-200 rounded-2xl"> Please Note as there is 20% penalty fee for every emergency withdrawal</p>
+      </div>
+      <button onClick={handleEmergencyWithdraw} className="mt-10 btn-secondary border-red-500 text-red-500 hover:bg-red-50 w-full h-12 rounded-lg font-semibold text-base">
         Emergency Withdraw
       </button>
     </div>
